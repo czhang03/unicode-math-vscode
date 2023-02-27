98245a7cb4b0daa143106c7fc2d2df7ebc6ed32d
CZhang <zcsx.ok@outlook.com>
2023-02-27 13:29:11 -0500

    reformatting and refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,9 @@
 import {
-    TextDocument, Position, CancellationToken, CompletionContext,
-    Range, CompletionItem, ExtensionContext, TextEditor,
+    TextDocument, Position, Range, CompletionItem, ExtensionContext, TextEditor,
     TextEditorEdit, Uri, commands, languages, window, CompletionItemKind
-}  from "vscode";
+} from "vscode";
 import { supsMap, subsMap, boldMap, italicMap, calMap, frakMap, bbMap } from "./maps";
-import {symbols} from './symbols';
+import { symbols } from './symbols';
 
 const SPACE_KEY: string = 'space';
 
@@ -18,20 +17,21 @@ export function activate(context: ExtensionContext) {
     // register the completion provider
     const completionProvider = languages.registerCompletionItemProvider(
         '*',
-        {provideCompletionItems(document: TextDocument, position: Position){
-            console.debug("completion triggered, evaluating current position...")
-            const [target, word] = evalPosition(document, position);
-            if (!target || !word) { return []; }
-            console.log(`trying to provide completion for ${word}`)
-            return genCompletions(word, target)
-        }},
+        {
+            provideCompletionItems(document: TextDocument, position: Position) {
+                console.debug("completion triggered, evaluating current position...")
+                const [target, word] = evalPosition(document, position);
+                if (!target || !word) { return []; }
+                console.log(`trying to provide completion for ${word}`)
+                return genCompletions(word, target)
+            }
+        },
         "\\"  // trigger completion on slash
     )
     context.subscriptions.push(completionProvider);
-    
-    // legacy code
-    const ctl = new UnicodeMaths(symbols);
-    context.subscriptions.push(commands.registerCommand('unicode-math-vscode.commit_tab', () => ctl.commit('tab')));
+
+    // register tab commit
+    context.subscriptions.push(commands.registerCommand('unicode-math-vscode.commit_tab', () => tabCommit('tab')));
     context.subscriptions.push(commands.registerCommand('unicode-math-symbols_html', () => {
         commands.executeCommand('open', Uri.parse('https://github.com/mvoidex/UnicodeMath/blob/master/table.md'));
     }));
@@ -48,13 +48,13 @@ export function deactivate() { }
  * Types of string mappings, usually used for math fonts
  */
 enum StringMapType {
-    Superscript,
-    Subscript,
-    Bold,
-    Italic,
-    MathCal,
-    MathFrak,
-    MathBB,
+    Superscript = "Superscript",
+    Subscript = "Subscript",
+    Bold = "Bold",
+    Italic = "Italic",
+    MathCal = "mathcal",
+    MathFrak = "mathfrak",
+    MathBB = "mathbb",
 }
 
 
@@ -93,25 +93,15 @@ const prefixes: string[] = Array.from(prefixToMapType.keys())
 
 // Give the map type its corresponding map.
 function mapTypeToMap(mapType: StringMapType): Map<string, string> {
-
-    const map: Map<StringMapType, Map<string, string>> = new Map([
-        [StringMapType.Superscript, supsMap],
-        [StringMapType.Subscript, subsMap],
-        [StringMapType.Bold, boldMap],
-        [StringMapType.Italic, italicMap],
-        [StringMapType.MathCal, calMap],
-        [StringMapType.MathFrak, frakMap],
-        [StringMapType.MathBB, bbMap],
-    ])
-
-    // add an additional check for log purpose
-    const res = map.get(mapType)
-    if (res === undefined) {
-        console.log("unexpected error, there is no map for mapType")
+    switch(mapType) {
+        case StringMapType.Superscript: return supsMap
+        case StringMapType.Subscript: return subsMap
+        case StringMapType.Bold: return boldMap
+        case StringMapType.Italic: return italicMap
+        case StringMapType.MathCal: return calMap
+        case StringMapType.MathFrak: return frakMap
+        case StringMapType.MathBB: return bbMap
     }
-
-    // if result is empty then give a empty map
-    return res ?? new Map()
 }
 
 
@@ -128,7 +118,7 @@ function stripPrefix(word: string): [StringMapType, string] | null {
     const wordWithoutPrefix = word.slice(longestPrefix.length)
     const mapType = prefixToMapType.get(longestPrefix)
 
-    return mapType? [mapType, wordWithoutPrefix] : null
+    return mapType ? [mapType, wordWithoutPrefix] : null
 }
 
 /**
@@ -147,19 +137,19 @@ function mapString(str: string, type: StringMapType): string {
 
 /**
  * Given a user inputted string, convert it into unicode, 
- * return undefined if it cannot be converted
+ * return null if it cannot be converted
  * @param str a input string, typed in the editor by the user
  * @returns the unicode version of the input string
  */
-function convertString(str: string) : string | null {
+function convertString(str: string): string | null {
 
     const [mapType, withoutPrefix] = stripPrefix(str) ?? [null, null]
-    
+
     // if a prefix cannot be found, then fallback to search in symbols
-    if (mapType === null && withoutPrefix === null) {return symbols[str] || null}
+    if (mapType === null && withoutPrefix === null) { return symbols[str] || null }
 
     // if prefix can be found, using prefix
-    else {return mapString(withoutPrefix, mapType)}
+    else { return mapString(withoutPrefix, mapType) }
 }
 
 
@@ -169,7 +159,7 @@ function convertString(str: string) : string | null {
  * @param target the range (starting position to end position) of the str including the slash in the beginning
  * @returns a list of completion items from the current string.
  */
-function genCompletions(str: string, target: Range): CompletionItem[]{
+function genCompletions(str: string, target: Range): CompletionItem[] {
     console.debug(`generating completion for ${str}`)
 
     // special case if the string matches any prefix
@@ -193,14 +183,14 @@ function genCompletions(str: string, target: Range): CompletionItem[]{
             const completion = new CompletionItem(prefix, CompletionItemKind.Keyword)
             completion.range = target
             // retrigger completion after prefix, to complete the map string
-            completion.command = 
-                { command: 'editor.action.triggerSuggest', title: 'completing after prefix'} 
+            completion.command =
+                { command: 'editor.action.triggerSuggest', title: 'completing after prefix' }
             return completion
         })
-        
-        const symbolCompletionsItems = 
+
+        const symbolCompletionsItems =
             Object.entries<string>(symbols).map(([inpStr, unicodeChar]) => {
-                const completion = new CompletionItem(inpStr, CompletionItemKind.Constant) 
+                const completion = new CompletionItem(inpStr, CompletionItemKind.Constant)
                 completion.detail = unicodeChar
                 completion.insertText = unicodeChar
                 completion.range = target
@@ -212,6 +202,7 @@ function genCompletions(str: string, target: Range): CompletionItem[]{
 }
 
 
+// legacy code
 // this implementation has a loser meaning of word (anything starting with \)
 function getWordRangeAtPosition(document: TextDocument, position: Position): [Range, string] {
     const lineStart = new Position(position.line, 0);
@@ -231,86 +222,46 @@ function evalPosition(document: TextDocument, position: Position): [Range, strin
         const [range, word] = getWordRangeAtPosition(document, position);
         return !word || !word.startsWith('\\') ? [null, null] : [range, word];
     } catch (e) {
+        console.error(e)
         return [null, null];
     }
 }
 
 
+function tabCommit(key: string): void {
+    if (!key || !window.activeTextEditor || !window.activeTextEditor.selection) { return; }
 
-class UnicodeMaths {
-    private readonly keys: string[];
-    constructor(private readonly codes: { [key: string]: string }) { this.keys = Object.keys(codes); }
-
-    public async provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): Promise<CompletionItem[]> {
-        const [target, word] = this.evalPosition(document, position);
-        if (!target || !word) { return []; }
-        const matches = this.keys.filter((k: string) => k.startsWith(word));
-        return matches.map((key: string) => {
-            const item = new CompletionItem(key);
-            item.detail = this.codes[key];
-            item.insertText = this.codes[key];
-            item.range = target;
-            return item;
-        });
-    }
-
-    public commit(key: string): void {
-        if (!key || !window.activeTextEditor || !window.activeTextEditor.selection) { return; }
-
-        const editor: TextEditor = <TextEditor>window.activeTextEditor;
-        const doKey = () => {
-            if (key === SPACE_KEY) {
-                commands.executeCommand('type', { source: 'keyboard', text: ' ' });
-            } else {
-                commands.executeCommand(key);
-            }
-        };
-
-        var c = false;
-        editor.edit((editor: TextEditorEdit) => {
-            if (!window.activeTextEditor) { return; }
-            window.activeTextEditor.selections.map((v) => {
-                const position = v.start;
-                if (window.activeTextEditor) {
-                    const [target, word] = this.evalPosition(window.activeTextEditor.document, position);
-                    if (target && word) {
-                        console.debug(`trying to commit ${word} from ${target.start} to ${target.end}`)
-                        const changed = convertString(word);
-                        console.debug(`committing to ${changed}`)
-                        if (changed) {
-                            editor.delete(target);
-                            editor.insert(target.start, changed);
-                            c = true;
-                        };
-                    }
-                }
-            });
-        });
-        // always propagate the space key, or propagate tab
-        // only if not used to insert a character
-        if (!c || key === SPACE_KEY) { return doKey(); }
-    }
-
-    private evalPosition(document: TextDocument, position: Position): [Range, string] | [null, null] {
-        if (position.character === 0) { return [null, null]; }
-        try {
-            const [range, word] = this.getWordRangeAtPosition(document, position);
-            return !word || !word.startsWith('\\') ? [null, null] : [range, word];
-        } catch (e) {
-            return [null, null];
+    const editor: TextEditor = <TextEditor>window.activeTextEditor;
+    const doKey = () => {
+        if (key === SPACE_KEY) {
+            commands.executeCommand('type', { source: 'keyboard', text: ' ' });
+        } else {
+            commands.executeCommand(key);
         }
-    }
+    };
 
-    // this implementation has a loser meaning of word (anything starting with \)
-    private getWordRangeAtPosition(document: TextDocument, position: Position): [Range, string] {
-        const lineStart = new Position(position.line, 0);
-        const lnRange = new Range(lineStart, position);
-        const line = document.getText(lnRange);
-        const slash = line.lastIndexOf('\\');
-        const word = line.slice(slash).trim();
-        const start = new Position(position.line, slash);
-        const end = start.translate(undefined, word.length);
-        return [new Range(start, end), word];
-    }
-
+    var c = false;
+    editor.edit((editor: TextEditorEdit) => {
+        window.activeTextEditor?.selections.map((v) => {
+            const position = v.start;
+            if (window.activeTextEditor) {
+                const [target, word] = evalPosition(window.activeTextEditor.document, position);
+                if (target && word) {
+                    console.debug(`trying to commit ${word}`)
+                    const changed = convertString(word);
+                    console.debug(`committing to ${changed}`)
+                    if (changed) {
+                        editor.delete(target);
+                        editor.insert(target.start, changed);
+                        c = true;
+                    };
+                }
+            }
+        });
+    });
+    // always propagate the space key, or propagate tab
+    // only if not used to insert a character
+    if (!c || key === SPACE_KEY) { return doKey(); }
 }
+
+

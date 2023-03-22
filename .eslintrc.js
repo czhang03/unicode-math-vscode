4b8a5c22b484926f016e008b800c8c2a3d863841
CZhang <zcsx.ok@outlook.com>
2023-03-22 11:27:14 -0400

    add extended rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "extends": [
         "prettier",
         'eslint:recommended', 
+        "plugin:jsdoc/recommended-error"
     ],
     "parser": "@typescript-eslint/parser",
     "overrides": [
@@ -60,31 +61,5 @@ module.exports = {
         "no-redeclare": "warn",
         "no-throw-literal": "warn",
         "no-unused-expressions": "warn",
-
-        // jsdoc 
-        "jsdoc/check-access": 1,  
-        "jsdoc/check-alignment": 1,  
-        "jsdoc/check-param-names": 1,  
-        "jsdoc/check-property-names": 1,  
-        "jsdoc/check-tag-names": 1,  
-        "jsdoc/check-values": 1,  
-        "jsdoc/empty-tags": 1,  
-        "jsdoc/implements-on-classes": 1,  
-        "jsdoc/multiline-blocks": 1,  
-        "jsdoc/newline-after-description": 1,  
-        "jsdoc/no-multi-asterisks": 1,  
-        "jsdoc/require-jsdoc": 1,  
-        "jsdoc/require-param": 1,  
-        "jsdoc/require-param-description": 1,  
-        "jsdoc/require-param-name": 1,  
-        "jsdoc/require-property": 1,  
-        "jsdoc/require-property-description": 1,  
-        "jsdoc/require-property-name": 1,  
-        "jsdoc/require-returns": 1,  
-        "jsdoc/require-returns-check": 1,  
-        "jsdoc/require-returns-description": 1,  
-        "jsdoc/require-yields": 1,  
-        "jsdoc/require-yields-check": 1,  
-        "jsdoc/tag-lines": 1,  
     }
 }
